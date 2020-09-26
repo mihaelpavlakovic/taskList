4082197baa9e8c42ef933ac692315d5f414daa0d
Mihael Pavlaković <mihael.pavlakovic@gmail.com>
2020-09-26 18:01:08 +0200

    Second commit

--- app.js
+++ app.js
@@ -102,12 +102,10 @@ function storeTaskInLocalStorage(task) {
 // Remove Task
 function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
-    if(confirm('Are You Sure?')) {
-      e.target.parentElement.parentElement.remove();
+    e.target.parentElement.parentElement.remove();
 
-      // Remove from local storage
-      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-    }
+    // Remove from local storage
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
   }
 }
 
